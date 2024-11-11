da55e26faedcab35e99892b887c65abc31a9a2da
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-11-12 01:03:22 +0400

    Initial Commit

---
+++ ColorsAppTests/ColorsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsAppTests.swift
+//  ColorsAppTests
+//
+//  Created by Hamida on 12.11.24.
+//
+
+import Testing
+@testable import ColorsApp
+
+struct ColorsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
